0c24c7701d04516a9091a8060c66c6459f5655af
Mikhail Malyshev <mlshv42@ya.ru>
2018-02-23 12:05:02 +0300

    Факультет добавлен в formData, описано вступильное испытание

--- src/submit.js
+++ src/submit.js
@@ -2,12 +2,13 @@ const submit = values => {
     const url = '/';
     const formData = new FormData();
 
-    const { firstname, lastname, github, telegram, files } = values;
+    const { firstname, lastname, github, telegram, faculty, files } = values;
 
     formData.append('firstname', firstname);
     formData.append('lastname', lastname);
     formData.append('github', github);
     formData.append('telegram', telegram);
+    formData.append('faculty', faculty);
     formData.append('screenshot', files[0]);
     return fetch(url, { method: 'POST', body: formData }).then(
         response => response.status === 200
